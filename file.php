c94bb92106e579b4cefdef58b4bda7d1cd6fc7c7
DIGIT <mondaynelson64@gmail.com>
2020-03-23 01:17:57 +0100

    just a project

---
+++ file.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <?php
+    $Name="John";
+    echo $Name;
+    ?>
+<br>
+<?php
+echo "Hello PHP";
+?>
+<br>
+<?php
+$a=5;
+echo "My Uncles Name is" .$Name;
+?>
+<br>
+<?php
+$listOfNoiseMakers="james";
+$listOfNoiseMakers.="Peter";
+$listOfNoiseMakers.="Thomas";
+$listOfNoiseMakers.="Judas";
+$listOfNoiseMakers.="Matthew";
+echo $listOfNoiseMakers;
+?>
+</body>
+</html>
